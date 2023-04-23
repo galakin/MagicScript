b9e73b27e1514924e2d7a732c6f2371e71cf332b
galakin <jacopo.pela@libero.it>
2023-04-23 19:40:20 +0200

    feat(testing): add init testing pipeline

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import os
 import pathlib
-import pandas  # TODO: change import location
+import pandas
 import json
 
 import src.priceCard as priceCard
 import src.rwCsw as rwCsw
+import src.connection as nwrk
 
 base_url = "https://api.cardtrader.com/api/v2"
 game = "Magic"
@@ -20,21 +21,9 @@ if os.getenv("AUTH_TOKEN") != None:
     print("...Auth token fetched from environment variables")
 
 else:
-    print("test 0")
     raise Exception("Unable to find auth token!")
 
 headers = {"Authorization": auth_token}
-
-
-def verify_connection():
-    ##Fetch game info
-    response = requests.get(base_url + "/info", headers=headers)
-    if response.status_code == 200:
-        print("...Connection Established!\n")
-    else:
-        raise Exception(
-            "Unable to connet to CardTrader API server\nCheck if your API token is still valid!"
-        )
 
 
 def fetch_local_card_data():
@@ -76,62 +65,16 @@ def preliminary_action():
     return True
 
 
-def search_for_card(name):
-    blueprint_id = -1
-    expansion_id = -1
-    expansion_name = ""
-
-    card_json = requests.get(database_url + "/cards/named?exact=" + name)
-    if card_json.status_code != 200:
-        exit()
-    scryfall_id = card_json.json()["id"]
-    card_expansion_code = card_json.json()["set"]
-    expansion_code = requests.get(base_url + "/expansions", headers=headers)
-    if expansion_code.status_code != 200:
-        exit()
-
-    print("...Retrived the list of expansions")
-    for elem in expansion_code.json():
-        if elem["code"] == card_expansion_code:
-            if expansion_id > 0:
-                exit()
-            expansion_id = elem["id"]
-            expansion_name = elem["code"]
-            print("...Card code found!")
-
-    # retrive card blueprint
-    card_blueprint = requests.get(
-        base_url + "/blueprints/export?expansion_id=" + str(expansion_id),
-        headers=headers,
-    )
-    if card_blueprint.status_code != 200:
-        raise Exception("unable to fetch expensions card")
-
-    for elem in card_blueprint.json():
-        if elem["name"] == name:
-            if blueprint_id > 0:
-                raise Exception("two product with the same name found")
-            blueprint_id = elem["id"]
-
-    selled_cards = requests.get(
-        base_url + "/marketplace/products?blueprint_id=" + str(blueprint_id),
-        headers=headers,
-    )
-    tst = list(selled_cards.json().keys())
-
-    print("...feteching prices info")
-    card_price = priceCard.get_prices(selled_cards.json(), tst[0])
-    # print(card_price)
-    rwCsw.write_to_csv(name, expansion_name, card_price)
-
-
-verify_connection()
-if preliminary_action():
-    print("Fetching card info...")
-    csv_file = rwCsw.read_csv()
-    try:
-        for elem in range(len(csv_file["card"])):
-            search_for_card(csv_file["card"][elem])
-    except Exception as ex:
-        print(ex)
-        exit()
+if __name__ == "__main__":
+    nwrk.verify_connection(base_url, headers)
+    if preliminary_action():
+        print("Fetching card info...")
+        csv_file = rwCsw.read_csv()
+        try:
+            for elem in range(len(csv_file["card"])):
+                nwrk.search_for_card(
+                    csv_file["card"][elem], database_url, base_url, headers
+                )
+        except Exception as ex:
+            print(ex)
+            exit()
--- test/test.py
+++ test/test.py
@@ -1,4 +1,16 @@
 test_threshold = 90
 
-import src.priceCard as priceCard
-import src.rwCsw as rwCsw
+import test_connection as connection
+
+total_test = 0
+successful_test = 0
+
+print("start testing ...")
+print("...test connection module")
+connection_tst = connection.test_connection()
+print(
+    "total test: ",
+    connection_tst["total"],
+    "successfull test: ",
+    connection_tst["successfull"],
+)
